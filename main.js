243b1900b2b74afa28bc7b51026a89f33290724f
Ctdlog <qpflapffhs76@gmail.com>
2022-01-28 20:52:28 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -41,3 +41,12 @@ function scrollIntoView(seletor) {
   const scrollTo = document.querySelector(seletor);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
