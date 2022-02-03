846197c071bfe9d35f5ccbf66d7518a64bb6e3db
Ctdlog <qpflapffhs76@gmail.com>
2022-02-03 22:05:13 +0900

    JS : Project filtering

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
@@ -35,13 +34,6 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-// Common Function
-
-function scrollIntoView(seletor) {
-  const scrollTo = document.querySelector(seletor);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
-}
-
 // Make home slowly fade to transparent as the window scrolls down
 
 const home = document.querySelector(".home__container");
@@ -50,3 +42,52 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Show "arrow up" button when scrolling down
+
+const arrowUp = document.querySelector(".arrow-up");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+// Handle click on the "arrow up" button
+
+arrowUp.addEventListener("click", () => {
+  scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// Filtering projects
+
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
+
+// Common Function
+
+function scrollIntoView(seletor) {
+  const scrollTo = document.querySelector(seletor);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
